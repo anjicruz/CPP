2ce07882b24df64d73e26255bd87a9bececb4ea1
anjicruz <anji_cruz@msn.com>
2021-01-15 15:41:33 +0800

    C++string part2 0115 341pm

--- C++string.cpp
+++ C++string.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
-
+#include <cstring>
 using namespace std;
 
 int main()
 {
-
-    char greeting[6] = {'H', 'e', 'l', 'l', 'o', '\0'};
-
-    cout << "Greeting message: ";
-    cout << greeting << endl;
-
+    char str1[10] = "Hello";
+    char str2[10] = "World";
+    char str3[10];
+    int len;
+    strcpy(str3, str1);
+    cout << "strcpy(str3,str1) : " << str3 << endl;
+    strcat(str1, str2);
+    cout << "strcat(str1,str2) : " << str1 << endl;
+    len = strlen(str1);
+    cout << "strlen(str1) : " << len << endl;
     return 0;
 }
