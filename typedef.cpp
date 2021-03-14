37fe1fbd4b48f3179811191a58732754c122eb84
Ji Cruz <29168882+anjicruz@users.noreply.github.com>
2021-03-14 19:56:42 +0800

    Update typedef.cpp

--- typedef.cpp
+++ typedef.cpp
@@ -17,4 +17,9 @@ int main()
     cout << "Length: " << Length << endl;
     cout << "Area: " << Area << endl;
     return 0;
-}
+}/*
+typedef is a keyword used in C language to assign alternative names to existing datatypes.
+Its mostly used with user defined datatypes, 
+when names of the datatypes become slightly complicated to use in programs.
+Following is the general syntax for using typedef ,
+typedef <existing_name> <alias_name>*/
