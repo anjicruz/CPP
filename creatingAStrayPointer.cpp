1144a6d1bf7804cade285b6f78a285f0207b8f3b
anjicruz <anji_cruz@msn.com>
2021-03-16 15:43:29 +0800

    Creating a Stray Pointer 0316 343pm

---
+++ creatingAStrayPointer.cpp
@@ -0,0 +1,26 @@
+//	LISTING 8.7 Creating a Stray Pointer
+
+typedef unsigned short int USHORT;
+#include <iostream>
+
+int main()
+{
+    using std::cin;
+    using std::cout;
+    using std::endl;
+    USHORT *pInt = new USHORT;
+    *pInt = 10;
+    cout << "*pInt: " << *pInt << endl;
+    delete pInt;
+
+    long *pLong = new long;
+    *pLong = 90000;
+    cout << "*pLong: " << *pLong << endl;
+
+    *pInt = 20; // uh oh, this was deleted
+
+    cout << "*pInt: " << *pInt << endl;
+    cout << "*pLong: " << *pLong << endl;
+    delete pLong;
+    return 0;
+}
